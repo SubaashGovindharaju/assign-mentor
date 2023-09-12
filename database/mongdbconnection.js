6c9fb3a598569b2e5b84583c2b9fb415adb36868
Subaash G <104092882+SubaashGovindharaju@users.noreply.github.com>
2023-09-12 11:10:57 +0530

    connect to cloud db

--- database/mongdbconnection.js
+++ database/mongdbconnection.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv';
+dotenv.config();
 const localMongoUrl='mongodb://localhost:27017/day-37'
+// const userName='subaashG';
+// const password='bjuMyuL3sBpkWLiY';
+
+const username = process.env.DB_USERNAME || '';
+const password = process.env.DB_PASSWORD || '';
+const clusterName=process.env.DB_CLUSTER || '';
+const dbName=process.env.DB_NAME || '';
+const cloudMongoUrl=`mongodb+srv://${username}:${password}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+
+// mongodb+srv://subaashG:<password>@cluster0.qzc4bmq.mongodb.net/?retryWrites=true&w=majority
 
 const connectToDb=async()=>{
     try{
-        await mongoose.connect(localMongoUrl,
+        await mongoose.connect(cloudMongoUrl,
             {
                 useNewUrlParser:true,
             });
